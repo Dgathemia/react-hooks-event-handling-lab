db1cd14e912a099f1df6c6682ed9edb57716f2f8
danielgathemia <danielgathemia01@gmail.com>
2022-10-03 10:46:13 +0300

    complete

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handleOnChange(){
+         console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div>
+            <input type={'Password'} onChange={handleOnChange}></input>
+        </div>
     )
 }
 
